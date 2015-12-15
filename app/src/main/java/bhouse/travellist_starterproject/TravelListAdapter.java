e0e690b6a215fd65a2e76e7ab2d1e4223c4e1da3
valentinerutto <rutto28@gmail.com>
2015-12-15 16:17:06 +0300

    initialized public variables in viewholderclas

--- app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
+++ app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by valentine on 12/15/15.
@@ -34,6 +37,10 @@ public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.Vi
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout placeHolder;
+        public LinearLayout placeNameHolder;
+        public TextView placeName;
+        public ImageView placeImage;
         public ViewHolder(View itemView) {
             super(itemView);
         }
