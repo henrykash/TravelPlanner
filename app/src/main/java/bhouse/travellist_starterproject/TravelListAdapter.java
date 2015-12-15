82b21a476c22cc4b72c3f5df20038ae09319c8e9
valentinerutto <rutto28@gmail.com>
2015-12-15 15:45:54 +0300

    travellistadapter class extends recyclerviewadapter and implemted viewholder methods

---
+++ app/src/main/java/bhouse/travellist_starterproject/TravelListAdapter.java
@@ -0,0 +1,32 @@
+package bhouse.travellist_starterproject;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by valentine on 12/15/15.
+ */
+// 1
+public class TravelListAdapter extends RecyclerView.Adapter<TravelListAdapter.ViewHolder> {
+
+
+
+    @Override
+    public TravelListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(TravelListAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+    }
+}
