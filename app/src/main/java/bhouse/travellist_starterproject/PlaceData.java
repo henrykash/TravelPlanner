408692f68f23bdd739e9ef8458f67cd148144a46
valentinerutto <rutto28@gmail.com>
2015-12-17 13:17:55 +0300

    changed placces names

--- app/src/main/java/bhouse/travellist_starterproject/PlaceData.java
+++ app/src/main/java/bhouse/travellist_starterproject/PlaceData.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class PlaceData {
 
-  public static String[] placeNameArray = {"Bora Bora", "Canada", "Dubai", "Hong Kong", "Iceland", "India", "Kenya", "London", "Switzerland", "Sydney"};
+  public static String[] placeNameArray = {"Bora-Bora", "Canada", "Dubai", "Hong Kong", "Iceland", "India", "Kenya", "London", "Switzerland", "Sydney"};
 
   public static ArrayList<Place> placeList() {
     ArrayList<Place> list = new ArrayList<>();
